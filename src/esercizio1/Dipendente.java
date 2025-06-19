6f8701f9285ab17cad45f708ab6b3697e7c78bfa
PaoloOliviero <paolo.oliviero3@gmail.com>
2025-06-19 16:40:56 +0200

    Commit 1

---
+++ src/esercizio1/Dipendente.java
@@ -0,0 +1,29 @@
+package esercizio1;
+
+public class Dipendente {
+    private String matricola;
+    private double stipendio;
+    private String dipartimento; // PRODUZIONE, AMMINISTRAZIONE, VENDITE
+
+    public Dipendente(String matricola, double stipendio, String dipartimento) {
+        this.matricola = matricola;
+        this.stipendio = stipendio;
+        this.dipartimento = dipartimento;
+    }
+
+    public String getMatricola() {
+        return matricola;
+    }
+
+    public double getStipendio() {
+        return stipendio;
+    }
+
+    public String getDipartimento() {
+        return dipartimento;
+    }
+
+    public void setDipartimento(String nuovoDipartimento) {
+        this.dipartimento = nuovoDipartimento;
+    }
+}
---
+++ src/esercizio1/Main.java
@@ -0,0 +1,24 @@
+package esercizio1;
+
+public class Main {
+    public static void main(String[] args) {
+        Dipendente d1 = new Dipendente("A123", 1800.50, "PRODUZIONE");
+        Dipendente d2 = new Dipendente("B456", 2200.00, "AMMINISTRAZIONE");
+        Dipendente d3 = new Dipendente("C789", 1950.75, "VENDITE");
+
+        Dipendente[] dipendenti = { d1, d2, d3 };
+
+        System.out.println("📋 Elenco iniziale dipendenti:");
+        for (Dipendente d : dipendenti) {
+            System.out.println("Matricola: " + d.getMatricola() + ", Dipartimento: " + d.getDipartimento());
+        }
+
+        d2.setDipartimento("VENDITE");
+
+        System.out.println("\n🔄 Dopo trasferimento del dipendente B456:");
+        for (Dipendente d : dipendenti) {
+            System.out.println("Matricola: " + d.getMatricola() + ", Dipartimento: " + d.getDipartimento());
+        }
+    }
+}
+
